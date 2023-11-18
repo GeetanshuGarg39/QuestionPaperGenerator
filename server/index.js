d73d09c15a9fcf23271b5e801d3643e5eb952427
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-11-18 13:29:58 +0530

    Connected MongoDb database

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import router from './routes/route.js';
 import cors from "cors";
 import dotenv from "dotenv";
+import ConnectDB from "./database/db.js";
 
 dotenv.config();
 const app = express(); 
@@ -15,10 +16,13 @@ app.use(bodyParser.urlencoded({extended:true}))
 // loading routes
 app.use('/', router); 
 
-const PORT = process.env.port || 8000 
+const PORT = process.env.PORT || 8000
+const MONGO_URL = process.env.MONGO_URL
 
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
 
 app.listen(PORT, console.log(`Server running at port ${PORT}`)); 
+
+ConnectDB(MONGO_URL);
