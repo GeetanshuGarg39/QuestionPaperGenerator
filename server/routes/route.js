0024d3b1a2617684cd86f95197ba9391d600969a
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-11-25 16:23:46 +0530

    Created generate paper endpoints

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,5 +1,10 @@
 import express from "express";
+import {  generatePaper } from "../controllers/questions-controller.js";
+
 
 const router = express.Router();
+
+// router.post("/addQuestions",addQuestions)
+router.post("/generate-paper",generatePaper)
 
 export default router;
