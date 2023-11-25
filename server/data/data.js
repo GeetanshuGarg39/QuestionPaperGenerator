c7f8d2e49cbd15ecb28a99467b956ef515ce09cd
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-11-25 16:22:44 +0530

    Added dummy data

---
+++ server/data/data.js
@@ -0,0 +1,402 @@
+export const data = [
+    {
+      "question": "What is the speed of light?",
+      "subject": "Science",
+      "topic": "Waves",
+      "difficulty": "easy",
+      "marks": 5
+    },
+    {
+      "question": "Chemical symbol for water?",
+      "subject": "Science",
+      "topic": "Elements",
+      "difficulty": "easy",
+      "marks": 4
+    },
+    {
+      "question": "Solve 2 + 2",
+      "subject": "Mathematics",
+      "topic": "Basic Arithmetic",
+      "difficulty": "easy",
+      "marks": 3
+    },
+    {
+      "question": "Who discovered penicillin?",
+      "subject": "Science",
+      "topic": "Biology",
+      "difficulty": "medium",
+      "marks": 6
+    },
+    {
+      "question": "Solve for x: 2x + 5 = 15",
+      "subject": "Mathematics",
+      "topic": "Algebra",
+      "difficulty": "medium",
+      "marks": 7
+    },
+    {
+      "question": "What is the structure of an atom?",
+      "subject": "Science",
+      "topic": "Chemistry",
+      "difficulty": "medium",
+      "marks": 8
+    },
+    {
+      "question": "Calculate the area of a rectangle with length 8 and width 6",
+      "subject": "Mathematics",
+      "topic": "Geometry",
+      "difficulty": "medium",
+      "marks": 5
+    },
+    {
+      "question": "Explain Newton's laws of motion.",
+      "subject": "Science",
+      "topic": "Physics",
+      "difficulty": "hard",
+      "marks": 10
+    },
+    {
+      "question": "Solve the quadratic equation: x^2 - 4x + 4 = 0",
+      "subject": "Mathematics",
+      "topic": "Algebra",
+      "difficulty": "hard",
+      "marks": 9
+    },
+    {
+      "question": "Define the concept of inertia.",
+      "subject": "Science",
+      "topic": "Physics",
+      "difficulty": "medium",
+      "marks": 6
+    },
+    {
+      "question": "Find the derivative of f(x) = 2x^2 + 3x + 1",
+      "subject": "Mathematics",
+      "topic": "Calculus",
+      "difficulty": "hard",
+      "marks": 8
+    },
+    {
+      "question": "Name the process of converting water vapor to liquid water.",
+      "subject": "Science",
+      "topic": "Chemistry",
+      "difficulty": "easy",
+      "marks": 4
+    },
+    {
+      "question": "Simplify the expression: (3x + 2)(x - 5)",
+      "subject": "Mathematics",
+      "topic": "Algebra",
+      "difficulty": "medium",
+      "marks": 7
+    },
+    {
+      "question": "What is the formula for the area of a circle?",
+      "subject": "Mathematics",
+      "topic": "Geometry",
+      "difficulty": "easy",
+      "marks": 5
+    },
+    {
+      "question": "Explain the process of photosynthesis.",
+      "subject": "Science",
+      "topic": "Biology",
+      "difficulty": "hard",
+      "marks": 9
+    },
+    {
+      "question": "Solve the system of equations: 2x + y = 8, 3x - 2y = 1",
+      "subject": "Mathematics",
+      "topic": "Algebra",
+      "difficulty": "medium",
+      "marks": 6
+    },
+    {
+      "question": "Name the planets in our solar system.",
+      "subject": "Science",
+      "topic": "Astronomy",
+      "difficulty": "easy",
+      "marks": 4
+    },
+    {
+      "question": "Evaluate the integral: ∫(3x^2 + 2) dx",
+      "subject": "Mathematics",
+      "topic": "Calculus",
+      "difficulty": "hard",
+      "marks": 10
+    },
+    {
+      "question": "What is the chemical formula for glucose?",
+      "subject": "Science",
+      "topic": "Chemistry",
+      "difficulty": "medium",
+      "marks": 7
+    },
+    {
+      "question": "Find the solution to the logarithmic equation: log₂(x) = 3",
+      "subject": "Mathematics",
+      "topic": "Algebra",
+      "difficulty": "hard",
+      "marks": 8
+    },
+    {
+      "question": "Explain the concept of natural selection.",
+      "subject": "Science",
+      "topic": "Biology",
+      "difficulty": "medium",
+      "marks": 6
+    },
+    {
+      "question": "Calculate the perimeter of a rectangle with length 12 and width 5",
+      "subject": "Mathematics",
+      "topic": "Geometry",
+      "difficulty": "easy",
+      "marks": 3
+    },
+    {
+      "question": "Define the law of conservation of energy.",
+      "subject": "Science",
+      "topic": "Physics",
+      "difficulty": "hard",
+      "marks": 9
+    },
+    {
+      "question": "Solve the trigonometric equation: sin(x) = 0.5",
+      "subject": "Mathematics",
+      "topic": "Trigonometry",
+      "difficulty": "medium",
+      "marks": 7
+    },
+    {
+      "question": "Name the four bases in DNA.",
+      "subject": "Science",
+      "topic": "Biology",
+      "difficulty": "easy",
+      "marks": 5
+    },
+    {
+      "question": "Find the value of cos(π/3)",
+      "subject": "Mathematics",
+      "topic": "Trigonometry",
+      "difficulty": "medium",
+      "marks": 6
+    },
+    {
+      "question": "Explain the concept of atomic mass.",
+      "subject": "Science",
+      "topic": "Chemistry",
+      "difficulty": "hard",
+      "marks": 8
+    },
+    {
+      "question": "Solve the equation: 4^x = 16",
+      "subject": "Mathematics",
+      "topic": "Algebra",
+      "difficulty": "easy",
+      "marks": 4
+    },
+    {
+      "question": "Define the law of reflection in optics.",
+      "subject": "Science",
+      "topic": "Physics",
+      "difficulty": "medium",
+      "marks": 7
+    },
+    {
+      "question": "Calculate the volume of a cylinder with radius 3 and height 8",
+      "subject": "Mathematics",
+      "topic": "Geometry",
+      "difficulty": "hard",
+      "marks": 10
+    },
+    {
+      "question": "What is the boiling point of water?",
+      "subject": "Science",
+      "topic": "Chemistry",
+      "difficulty": "easy",
+      "marks": 5
+    },
+    {
+      "question": "Solve the system of equations: 3x + 2y = 10, 2x - y = 3",
+      "subject": "Mathematics",
+      "topic": "Algebra",
+      "difficulty": "medium",
+      "marks": 8
+    },
+    {
+      "question": "Explain the concept of mitosis.",
+      "subject": "Science",
+      "topic": "Biology",
+      "difficulty": "hard",
+      "marks": 9
+    },
+    {
+      "question": "Find the roots of the quadratic equation: x^2 - 7x + 12 = 0",
+      "subject": "Mathematics",
+      "topic": "Algebra",
+      "difficulty": "medium",
+      "marks": 7
+    },
+    {
+      "question": "Define the law of supply and demand in economics.",
+      "subject": "Science",
+      "topic": "Economics",
+      "difficulty": "medium",
+      "marks": 6
+    },
+    {
+      "question": "Calculate the mean of the numbers: 5, 8, 12, 15, 20",
+      "subject": "Mathematics",
+      "topic": "Statistics",
+      "difficulty": "easy",
+      "marks": 4
+    },
+    {
+      "question": "Explain the concept of heat transfer.",
+      "subject": "Science",
+      "topic": "Physics",
+      "difficulty": "medium",
+      "marks": 7
+    },
+    {
+      "question": "Simplify the expression: 2(3x - 4) + 5x",
+      "subject": "Mathematics",
+      "topic": "Algebra",
+      "difficulty": "easy",
+      "marks": 3
+    },
+    {
+      "question": "Name the elements in the periodic table.",
+      "subject": "Science",
+      "topic": "Chemistry",
+      "difficulty": "hard",
+      "marks": 10
+    },
+    {
+      "question": "Evaluate the expression: sin(π/4) + cos(π/3)",
+      "subject": "Mathematics",
+      "topic": "Trigonometry",
+      "difficulty": "medium",
+      "marks": 6
+    },
+    {
+      "question": "Define the concept of a derivative in calculus.",
+      "subject": "Science",
+      "topic": "Calculus",
+      "difficulty": "hard",
+      "marks": 8
+    },
+    {
+      "question": "Solve the inequality: 2x - 5 > 8",
+      "subject": "Mathematics",
+      "topic": "Algebra",
+      "difficulty": "easy",
+      "marks": 5
+    },
+    {
+      "question": "Explain the process of meiosis.",
+      "subject": "Science",
+      "topic": "Biology",
+      "difficulty": "medium",
+      "marks": 7
+    },
+    {
+      "question": "Find the area of a triangle with base 6 and height 9",
+      "subject": "Mathematics",
+      "topic": "Geometry",
+      "difficulty": "hard",
+      "marks": 9
+    },
+    {
+      "question": "Define the law of conservation of mass.",
+      "subject": "Science",
+      "topic": "Chemistry",
+      "difficulty": "medium",
+      "marks": 6
+    },
+    {
+      "question": "Solve the trigonometric equation: cos(2x) = -1",
+      "subject": "Mathematics",
+      "topic": "Trigonometry",
+      "difficulty": "hard",
+      "marks": 8
+    },
+    {
+      "question": "Explain the process of cellular respiration.",
+      "subject": "Science",
+      "topic": "Biology",
+      "difficulty": "medium",
+      "marks": 6
+    },
+    {
+      "question": "Calculate the surface area of a sphere with radius 4",
+      "subject": "Mathematics",
+      "topic": "Geometry",
+      "difficulty": "easy",
+      "marks": 4
+    },
+    {
+      "question": "What is the chemical formula for table salt?",
+      "subject": "Science",
+      "topic": "Chemistry",
+      "difficulty": "easy",
+      "marks": 3
+    },
+    {
+      "question": "Solve the linear equation: 3y - 2 = 7",
+      "subject": "Mathematics",
+      "topic": "Algebra",
+      "difficulty": "medium",
+      "marks": 7
+    },
+    {
+      "question": "Explain the concept of force in physics.",
+      "subject": "Science",
+      "topic": "Physics",
+      "difficulty": "hard",
+      "marks": 10
+    },
+    {
+      "question": "Evaluate the integral: ∫(e^x) dx",
+      "subject": "Mathematics",
+      "topic": "Calculus",
+      "difficulty": "hard",
+      "marks": 9
+    },
+    {
+      "question": "Solve the logarithmic equation: log₃(x) = 2",
+      "subject": "Mathematics",
+      "topic": "Algebra",
+      "difficulty": "medium",
+      "marks": 6
+    },
+    {
+      "question": "Explain the concept of a limit in calculus.",
+      "subject": "Science",
+      "topic": "Calculus",
+      "difficulty": "hard",
+      "marks": 8
+    },
+    {
+      "question": "Calculate the mean deviation of the numbers: 4, 7, 10, 14, 18",
+      "subject": "Mathematics",
+      "topic": "Statistics",
+      "difficulty": "medium",
+      "marks": 6
+    },
+    {
+      "question": "What is the function of the nucleus in a cell?",
+      "subject": "Science",
+      "topic": "Biology",
+      "difficulty": "easy",
+      "marks": 5
+    },
+    {
+      "question": "Solve the quadratic equation: 2x^2 - 5x + 2 = 0",
+      "subject": "Mathematics",
+      "topic": "Algebra",
+      "difficulty": "medium",
+      "marks": 7
+    }
+  ]
+  
